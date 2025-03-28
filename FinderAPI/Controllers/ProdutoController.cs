6247fe54ebe83228fd55c6be581a06369e413d85
Gabriel Zanguettin Costa <gabrielzanguettintecnologia@gmail.com>
2025-03-28 15:24:16 -0300

    Add files via upload

---
+++ FinderAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using FinderAPI.Entidades;
+using FinderAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinderAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoService produtoService;
+        public ProdutoController(ProdutoService produtoService)
+        {
+            this.produtoService = produtoService;
+        }
+        [HttpPost("produto")]
+        public async Task<IActionResult> RegistrarProdutos([FromBody] List<Produto> produtos)
+        {
+            var produtosCadastrados = await produtoService.CadastrarProdutos(produtos);
+            return Ok(produtosCadastrados);
+        }
+    }
+}
